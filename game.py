815cf095533d27b86c6619c85cc8ee1cac0105ec
kithsaengnim <kithsaengnim@gmail.com>
2025-07-29 01:15:08 +0700

    Implement the get_player_guess and check_guess functions

--- game.py
+++ game.py
@@ -1,15 +1,25 @@
 import random
 
 def get_player_guess():
-    pass  # TODO: implement this function
+    return int(input("Enter your guess (1-100): "))
 
 def check_guess(secret, guess):
-    pass  # TODO: implement this function
+    if guess < secret:
+        print("Too low!")
+    elif guess > secret:
+        print("Too high!")
+    else:
+        print("Correct! You win!")
 
 def play_game():
+    print("Starting game...")  
     secret_number = random.randint(1, 100)
     guess = None
 
     while guess != secret_number:
         guess = get_player_guess()
         check_guess(secret_number, guess)
+
+if __name__ == "__main__":
+    print("Running from main")  
+    play_game()
